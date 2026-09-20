b260a53ceb1f3bcf58a0dc6162603d1c328efd3f
Danil Momot <danya541@bk.ru>
2026-09-20 19:20:09 +0300

    feat: arc cache update
    - Storing data via a unique_ptr
    - Delete the extra search in the entries_table
    - Moved some structures and fields to the private part of the ArcCache class

--- arc/include/arc.hpp
+++ arc/include/arc.hpp
@@ -10,7 +10,7 @@
 #include <cstddef>
 #include <iterator>
 #include <list>
-#include <optional>
+#include <memory>
 #include <stdexcept>
 #include <unordered_map>
 
@@ -34,31 +34,16 @@ namespace cache {
  *                 B2 - ghost entries recently removed from T2.
  *
  * The algorithm was completely taken from the original article about the ARC cache:
- * https://www.usenix.org/conference/fast-03/arc-self-tuning-low-overhead-replacement-cache 
+ * https://www.usenix.org/conference/fast-03/arc-self-tuning-low-overhead-replacement-cache
  */
 template <typename KeyT, typename DataT>
 class ArcCache {
 public:
-    using PoolT = typename std::list<KeyT>;
-    using IterT = typename PoolT::iterator;
-    
-    enum Pools { T1, T2, B1, B2 };
-
     struct LookupResult {
         LookupResult(const DataT& data, bool hit) : data_(data), hit_(hit) {}
 
         const DataT& data_; 
         bool hit_;
-    };
-
-    struct Entry {
-        Pools pool_;
-        IterT it_;
-        std::optional<DataT> data_;
-
-        Entry(Pools pool, IterT it) : 
-            pool_(pool), it_(it), data_(std::nullopt) 
-        {}
     };
 
     explicit ArcCache(std::size_t size) : cache_size_(size)
@@ -82,43 +67,53 @@ public:
      * @tparam FuncT Type of the data-loading callable.
      *
      * @param key Key of the requested cache entry.
-     * @param slow_get_page Callable used to load data on a cache miss.
+     * @param loader Callable used to load data on a cache miss.
      *
      * @return true if the requested data was already present in T1 or T2;
      *         false otherwise.   
     */
     template <typename FuncT>
-    LookupResult lookup_update(const KeyT& key, FuncT& slow_get_page) {
-        auto table_it = table_.find(key);
-        bool hit = false;
+    LookupResult lookup_update(const KeyT& key, FuncT& loader) {
+        auto entry_it = entries_table_.find(key);
 
-        if(table_it == table_.end())
-            case_IV(key, slow_get_page);
+        if(entry_it == entries_table_.end())
+            return case_IV(key, loader);
 
-        else if(table_it->second.pool_ == Pools::B2)
-            case_III(table_it->second, key, slow_get_page);
+        else if(entry_it->second.pool_ == Pools::B2)
+            return case_III(entry_it->second, key, loader);
 
-        else if(table_it->second.pool_ == Pools::B1)
-            case_II(table_it->second, key, slow_get_page);
+        else if(entry_it->second.pool_ == Pools::B1)
+            return case_II(entry_it->second, key, loader);
 
-        else {
-            case_I(table_it->second);
-            hit = true;
-        }
-
-        return {table_.at(key).data_.value(), hit};
+        return case_I(entry_it->second);
     }
 
 private:
+    using PoolT = std::list<KeyT>;
+    using IterT = PoolT::iterator;
+    using DataptrT = std::unique_ptr<DataT>;
+
+    enum Pools { T1, T2, B1, B2 };
+
+    struct Entry {
+        std::unique_ptr<DataT> data_;
+        IterT it_;
+        Pools pool_;
+
+        Entry(DataptrT data, Pools pool, IterT it) : 
+            data_(std::move(data)), it_(it), pool_(pool)
+        {}
+    };
+
     /**
      * @brief ARC pools are ordered from MRU to LRU:
      *        begin() is the most recently used element,
      *        back() is the least recently used element.
      */
-    std::list<KeyT> T1_{}, T2_{}, B1_{}, B2_{};
+    PoolT T1_{}, T2_{}, B1_{}, B2_{};
     
     /** @brief set of entries. */
-    std::unordered_map<KeyT, Entry> table_{};
+    std::unordered_map<KeyT, Entry> entries_table_{};
 
     /** @brief Maximum number of resident entries in T1 and T2 in total. */
     std::size_t cache_size_ = 0;
@@ -130,17 +125,19 @@ private:
      */
     std::size_t p_coeff_ = 0;
 
-    void case_I(Entry& entry)
+    LookupResult case_I(Entry& entry)
     { 
         auto& src = entry.pool_ == Pools::T1 ? T1_ : T2_;
         
         // Move x to T2 head
         T2_.splice(T2_.begin(), src, entry.it_);
         entry.pool_ = Pools::T2;
+
+        return {*entry.data_, true};
     }
 
     template <typename FuncT>
-    void case_II(Entry& entry, const KeyT& key, FuncT& slow_get_page)
+    LookupResult case_II(Entry& entry, const KeyT& key, FuncT& loader)
     {
         const std::size_t delta1 = B1_.size() >= B2_.size() ?
             1 : B2_.size() / B1_.size();
@@ -149,11 +146,13 @@ private:
         
         replace(false);
 
-        return_from_ghost_pool(entry, key, slow_get_page, B1_);
+        return_from_ghost_pool(entry, key, loader, B1_);
+
+        return {*entry.data_, false};
     }
 
     template <typename FuncT>
-    void case_III(Entry& entry, const KeyT& key, FuncT& slow_get_page)
+    LookupResult case_III(Entry& entry, const KeyT& key, FuncT& loader)
     {
         const std::size_t delta2 = B2_.size() >= B1_.size() ? 
             1 : B1_.size() / B2_.size();
@@ -162,18 +161,22 @@ private:
 
         replace(true);
 
-        return_from_ghost_pool(entry, key, slow_get_page, B2_);
+        return_from_ghost_pool(entry, key, loader, B2_);
+
+        return {*entry.data_, false};
     }
 
     template <typename FuncT>
-    void case_IV(const KeyT& key, FuncT& slow_get_page)
+    LookupResult case_IV(const KeyT& key, FuncT& loader)
     {
         if((T1_.size() + B1_.size()) == cache_size_)
             case_IV_A();
         else
             case_IV_B();
 
-        add_entry(key, slow_get_page);
+        auto entry_it = add_entry(key, loader);
+
+        return {*entry_it->second.data_, false};
     }
 
     void case_IV_A()
@@ -216,27 +219,35 @@ private:
     }
 
     template <typename FuncT>
-    void add_entry(const KeyT& key, FuncT& slow_get_page)
+    auto add_entry(const KeyT& key, FuncT& loader)
     {
-        // Put x in MRU T1 and save info in table
+        // Put x in MRU T1 and save info in entries table
         T1_.emplace_front(key);
 
-        auto [table_it, inserted] =
-            table_.try_emplace(key, Pools::T1, T1_.begin());
-        assert(inserted);
+        auto [entry_it, inserted] =
+            entries_table_.try_emplace(
+                key,
+                std::make_unique<DataT>(),
+                Pools::T1, T1_.begin()
+            );
         
-        slow_get_page(key, table_it->second.data_.emplace());
+        if(!inserted) throw std::runtime_error("Not enough memory to add new entry");
+        
+        loader(key, *(entry_it->second.data_));
+
+        return entry_it;
     }
 
     template <typename FuncT>
     void return_from_ghost_pool(Entry& entry, const KeyT& key,
-        FuncT& slow_get_page, PoolT& src)
+        FuncT& loader, PoolT& src)
     {
         // Move x from B1/B2 to the MRU of T2
         T2_.splice(T2_.begin(), src, entry.it_);
         entry.pool_ = Pools::T2;
 
-        slow_get_page(key, entry.data_.emplace());
+        entry.data_ = std::make_unique<DataT>();
+        loader(key, *entry.data_);
     }
 
     void remove_lru_from_cache(PoolT& src)
@@ -244,22 +255,22 @@ private:
         assert(!src.empty());
 
         auto src_it = std::prev(src.end());
-        table_.erase(*src_it);
+        entries_table_.erase(*src_it);
         src.pop_back();
     }
 
-    void evict_to_ghost_pool(PoolT& src, PoolT& dest, Pools dest_name)
+    void evict_to_ghost_pool(PoolT& src, PoolT& dest, const Pools dest_name)
     {
         assert(!src.empty());
 
         // Move from src to dest(ghost pool)
         dest.splice(dest.begin(), src, std::prev(src.end()));
         
-        // Erase data from cache and change entry in table
-        auto& table_victim = table_.at(*dest.begin());
+        // Erase data from cache and change entry in entries table
+        auto& entry_victim = entries_table_.at(*dest.begin());
 
-        table_victim.pool_ = dest_name;
-        table_victim.data_.reset();
+        entry_victim.pool_ = dest_name;
+        entry_victim.data_.reset();
     }
 
 }; // class ArcCache
